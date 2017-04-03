a267ac9a07bd13f2d177582b10723c4f61da5f4a
Javier Bórquez <hi@javier.xyz>
2017-04-02 19:48:55 -0500

    Prettier linting and fix

--- src/components/ImageAsCanvas.js
+++ src/components/ImageAsCanvas.js
@@ -1,46 +1,43 @@
-import React from 'react'
+import React from 'react';
 
 var ImageAsCanvas = React.createClass({
-
   propTypes: {
     base64image: React.PropTypes.string
   },
 
-  shouldComponentUpdate (nextProps) {
-    return nextProps.base64image !== this.props.base64image
+  shouldComponentUpdate(nextProps) {
+    return nextProps.base64image !== this.props.base64image;
   },
 
-  componentDidMount () {
-    if (this.props.base64image) this._loadImageToCanvas()
+  componentDidMount() {
+    if (this.props.base64image) this._loadImageToCanvas();
   },
 
-  componentDidUpdate () {
-    this._loadImageToCanvas()
+  componentDidUpdate() {
+    this._loadImageToCanvas();
   },
 
-  _loadImageToCanvas () {
-    var img = new window.Image()
+  _loadImageToCanvas() {
+    var img = new window.Image();
     img.onload = () => {
-      var canvas = React.findDOMNode(this.refs.canvas)
-      canvas.width = img.width
-      canvas.height = img.height
-      var ctx = canvas.getContext('2d')
-      ctx.drawImage(img, 0, 0)
+      var canvas = React.findDOMNode(this.refs.canvas);
+      canvas.width = img.width;
+      canvas.height = img.height;
+      var ctx = canvas.getContext('2d');
+      ctx.drawImage(img, 0, 0);
 
-      console.warn(ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height))
+      console.warn(ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height));
 
       // do some manipulations...
 
       // canvas.toDataURL('image/png')
-    }
-    img.src = this.props.base64image
+    };
+    img.src = this.props.base64image;
   },
 
-  render () {
-    return (
-      <canvas ref='canvas' />
-    )
+  render() {
+    return <canvas ref="canvas" />;
   }
-})
+});
 
-module.exports = ImageAsCanvas
+module.exports = ImageAsCanvas;
