49aa5463fd489e375f2f47a2401a6f7e92caf963
jb <hi@javier.xyz>
2020-10-11 06:05:12 +0200

    Remove dist, add image resize

--- src/app.jsx
+++ src/app.jsx
@@ -35,18 +35,49 @@ function compressColor(rgb) {
   return hex[1] === hex[2] && hex[3] === hex[4] && hex[5] === hex[6] ? "#" + hex[1] + hex[3] + hex[5] : hex;
 }
 
+function resizeImage(base64Str, maxWidth = 400, maxHeight = 350) {
+  return new Promise((resolve) => {
+    let img = new Image();
+    img.src = base64Str;
+    img.onload = () => {
+      let canvas = document.createElement("canvas");
+      const MAX_WIDTH = maxWidth;
+      const MAX_HEIGHT = maxHeight;
+      let width = img.width;
+      let height = img.height;
+
+      if (width > height) {
+        if (width > MAX_WIDTH) {
+          height *= MAX_WIDTH / width;
+          width = MAX_WIDTH;
+        }
+      } else {
+        if (height > MAX_HEIGHT) {
+          width *= MAX_HEIGHT / height;
+          height = MAX_HEIGHT;
+        }
+      }
+      canvas.width = width;
+      canvas.height = height;
+      let ctx = canvas.getContext("2d");
+      ctx.drawImage(img, 0, 0, width, height);
+      resolve(canvas.toDataURL());
+    };
+  });
+}
+
 function App() {
   const [rgbMatrix, rgbMatrixSet] = useState(null);
   const [loadingImage, loadingImageSet] = useState(false);
 
-  function onDrop(event) {
+  function onFileSelected(event) {
     event.stopPropagation();
     event.preventDefault();
 
     console.log(event);
 
-    let dt = event.dataTransfer;
-    let files = dt ? dt.files : event.target.files;
+    const dt = event.dataTransfer;
+    const files = dt ? dt.files : event.target.files;
     const file = files[0];
 
     // const file = dt.file;
@@ -55,17 +86,10 @@ function App() {
 
     loadingImageSet(true);
 
-    fr.onload = (data) => {
-      const base64 = data.currentTarget.result;
+    fr.onload = async (data) => {
+      const base64src = data.currentTarget.result;
 
-      if (base64.length > 100000) {
-        let confirmation = window.confirm("Your image is really big, do you really want to try to convert it?");
-
-        if (!confirmation) {
-          loadingImageSet(false);
-          return;
-        }
-      }
+      const base64 = await resizeImage(base64src, 128, 128);
 
       base64ImageToRGBMatrix(base64, (err, data) => {
         if (err) return console.error(err);
@@ -98,9 +122,9 @@ function App() {
 
   return (
     <div className="padding-horizontal-2x">
-      <div className="dropzone" onDrop={onDrop} onDragOver={onDragOver} onDragEnter={onDragOver}>
+      <div className="dropzone" onDrop={onFileSelected} onDragOver={onDragOver} onDragEnter={onDragOver}>
         <span>{loadingImage ? "Processing..." : "Drop an image here, or click to upload."}</span>
-        <input type="file" onChange={onDrop} multiple accept="image/*" />
+        <input type="file" onChange={onFileSelected} multiple accept="image/*" />
       </div>
 
       {rgbMatrix && (
