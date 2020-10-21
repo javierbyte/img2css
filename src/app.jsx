3073e3c1bdfc57bf1a2b307acd1ac7300d9858e0
jb <hi@javier.xyz>
2020-10-21 02:23:13 +0200

    Update dependencies

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,28 @@
-import React, { useState, useCallback } from "react";
+import React, { useState } from "react";
 import tinycolor from "tinycolor2";
 import _ from "lodash";
 
+import { HeaderH1, HeaderH2, Text, Space, Box, A, Container, Dropzone, Ul, Li } from "./jbx.jsx";
+
+import Styled from "styled-components";
+
+const Textarea = Styled.textarea({
+  "font-family": "monaco, monospace",
+  border: "none",
+  width: "100%",
+  height: "256px",
+  "font-size": "13px",
+  "line-height": "1.309",
+  padding: "16px 18px",
+  background: "#ecf0f1",
+  color: "#34495e",
+  ":focus": {
+    outline: "none",
+  },
+});
+
 import ImageUtils from "base64-image-utils";
 const base64ImageToRGBMatrix = ImageUtils.base64ImageToRGBMatrix;
-
-// import { base64ImageToRGBMatrix } from "base64-image-utils";
 
 function compressColor(rgb) {
   const hex = tinycolor(rgb).toHexString();
@@ -35,33 +52,32 @@ function compressColor(rgb) {
   return hex[1] === hex[2] && hex[3] === hex[4] && hex[5] === hex[6] ? "#" + hex[1] + hex[3] + hex[5] : hex;
 }
 
-function resizeImage(base64Str, maxWidth = 400, maxHeight = 350) {
+function resizeImage(base64Str, maxMass = 128 * 128) {
   return new Promise((resolve) => {
     let img = new Image();
     img.src = base64Str;
     img.onload = () => {
       let canvas = document.createElement("canvas");
-      const MAX_WIDTH = maxWidth;
-      const MAX_HEIGHT = maxHeight;
+
+      const originalWidth = img.width;
+      const originalHeight = img.height;
+
       let width = img.width;
       let height = img.height;
 
-      if (width > height) {
-        if (width > MAX_WIDTH) {
-          height *= MAX_WIDTH / width;
-          width = MAX_WIDTH;
-        }
-      } else {
-        if (height > MAX_HEIGHT) {
-          width *= MAX_HEIGHT / height;
-          height = MAX_HEIGHT;
-        }
+      while (width * height > maxMass) {
+        width = width / Math.sqrt(2, 2);
+        height = height / Math.sqrt(2, 2);
       }
+
+      width = Math.round(width);
+      height = Math.round(height);
+
       canvas.width = width;
       canvas.height = height;
       let ctx = canvas.getContext("2d");
       ctx.drawImage(img, 0, 0, width, height);
-      resolve(canvas.toDataURL());
+      resolve([canvas.toDataURL(), { originalWidth, originalHeight }]);
     };
   });
 }
@@ -85,12 +101,10 @@ function App() {
     fr.onload = async (data) => {
       const base64src = data.currentTarget.result;
 
-      const base64 = await resizeImage(base64src, 128, 128);
+      const [base64] = await resizeImage(base64src);
 
       base64ImageToRGBMatrix(base64, (err, data) => {
         if (err) return console.error(err);
-
-        console.log(data);
 
         rgbMatrixSet(data);
         loadingImageSet(false);
@@ -104,10 +118,18 @@ function App() {
     event.preventDefault();
   }
 
-  const masterShadow = _.map(rgbMatrix, (row, rowIndex) => {
-    return _.map(row, (col, colIndex) => {
+  let scale = 1;
+
+  const masterShadow = _.map(rgbMatrix, (row, rowIndexSrc) => {
+    return _.map(row, (col, colIndexSrc) => {
+      const colIndex = colIndexSrc * scale;
+      const rowIndex = rowIndexSrc * scale;
+
       const color = compressColor(`rgb(${col.r},${col.g},${col.b})`);
-      return `${color} ${colIndex ? colIndex + "px" : 0} ${rowIndex ? rowIndex + "px" : 0}`;
+
+      const scaleCompensation = scale !== 1 ? ` 0 ${scale / 2}px` : ``;
+
+      return `${color} ${colIndex ? colIndex + "px" : 0} ${rowIndex ? rowIndex + "px" : 0}${scaleCompensation}`;
     }).join(",");
   }).join(",");
 
@@ -118,38 +140,108 @@ function App() {
   };
 
   return (
-    <div className="padding-horizontal-2x">
-      <div className="dropzone" onDrop={onFileSelected} onDragOver={onDragOver} onDragEnter={onDragOver}>
-        <span>{loadingImage ? "Processing..." : "Drop an image here, or click to upload."}</span>
-        <input type="file" onChange={onFileSelected} multiple accept="image/*" />
-      </div>
+    <Container>
+      <Box padding={2}>
+        <Space h={1} />
 
-      {rgbMatrix && (
-        <div>
-          <div className="tutorial">
-            <h3>The result</h3>
-            This is your pure css (and single div) image! Enjoy!
+        <HeaderH1
+          style={{
+            fontWeight: 900,
+            display: "inline-block",
+            width: "auto",
+            padding: "6px",
+            backgroundColor: "var(--accent-color)",
+          }}>
+          img2css
+        </HeaderH1>
+
+        <Space h={1} />
+        <Text>This is a tool that can convert any image into a pure css image.</Text>
+        <Space h={2} />
+
+        <Dropzone onDrop={onFileSelected} onDragOver={onDragOver} onDragEnter={onDragOver}>
+          {loadingImage ? (
+            <Text>Processing...</Text>
+          ) : (
+            <div>
+              <Text>Drop an image here</Text>
+              <Text>or click to select</Text>
+            </div>
+          )}
+          <input
+            type="file"
+            onChange={onFileSelected}
+            multiple
+            accept="image/*"
+            aria-label="Drop an image here, or click to select"
+          />
+        </Dropzone>
+
+        <Space h={2} />
+        <Text>
+          I also made a per-pixel animation experiment, see <A href="https://javier.xyz/morphin/">morphin</A>.
+        </Text>
+
+        {rgbMatrix && (
+          <div>
+            <Space h={2} />
+            <HeaderH2>The result</HeaderH2>
+            <Space h={1} />
+            <Text>This is your pure css (and single div) image! Enjoy!</Text>
+            <Space h={1} />
             <div
-              className="pixel"
               style={{
                 height: 1,
                 width: 1,
                 boxShadow: masterShadow,
-                marginBottom: rgbMatrix.length,
-                marginRight: rgbMatrix[0].length,
+                marginBottom: rgbMatrix.length * scale,
+                marginRight: rgbMatrix[0].length * scale,
               }}
             />
+            <Space h={1} />
+            <Textarea
+              onFocus={handleFocus}
+              onChange={() => {}}
+              className="code"
+              value={`<div style="margin-right: ${rgbMatrix[0].length * scale}px; margin-bottom: ${
+                rgbMatrix.length * scale
+              }px; height: 1px; width: 1px; box-shadow: ${masterShadow}"></div>`}
+            />
+            <Space h={1} />
+            <Text>Size: {masterShadow.length.toLocaleString()}b</Text>
           </div>
-          <textarea
-            onFocus={handleFocus}
-            onChange={(e) => {}}
-            className="code"
-            value={`<div style="margin-right: ${rgbMatrix[0].length}px; margin-bottom: ${rgbMatrix.length}px; height: 1px; width: 1px; box-shadow: ${masterShadow}"></div>`}
-          />
-          Size: {masterShadow.length.toLocaleString()}b
-        </div>
-      )}
-    </div>
+        )}
+
+        <Space h={2} />
+
+        <Text>More unrelated experiments</Text>
+        <Space h={0.5} />
+        <Ul>
+          <Li>
+            <Text>
+              Play with 3D and shaws, <A href="https://sombras.app/">sombras.app</A>.
+            </Text>
+          </Li>
+          <Li>
+            <Text>
+              Find the visual center of your images / logos,{" "}
+              <A href="https://javier.xyz/visual-center/">visual-center</A>.
+            </Text>
+          </Li>
+          <Li>
+            <Text>
+              JS AI Battle Game, <A href="https://clashjs.com/">clashjs</A>.
+            </Text>
+          </Li>
+        </Ul>
+
+        <Space h={2} />
+        <Text>
+          Made by <A href="https://javier.xyz">javierbyte</A>.
+        </Text>
+        <Space h={3} />
+      </Box>
+    </Container>
   );
 }
 
